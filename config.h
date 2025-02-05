454d17f4acac8b18279495c344db3286186bd926
personinblack <berkay@tuta.io>
2025-02-05 18:12:20 +0300

    specify the font size as 11

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "cozette";
+static char *font = "cozette:size=11";
 static char *font2[] = { "Symbola:size=10" };
 static int borderpx = 3;
 
